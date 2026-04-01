fc9ab96f9b588bcea4f98c9bfe1c24350c78ebf2
傅逸凡 <fyf.yf@qq.com>
2026-04-01 18:35:25 +0800

    xcode工程添加watchApp

---
+++ iosApp/watchApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  watchApp Watch App
+//
+//  Created by 傅逸凡 on 2026/4/1.
+//
+
+import Shared
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosApp/watchApp/watchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchAppApp.swift
+//  watchApp Watch App
+//
+//  Created by 傅逸凡 on 2026/4/1.
+//
+
+import SwiftUI
+
+@main
+struct watchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
